bcd7a3b715211378de31e9fc8c0351a17fc18420
lorenzdiscutido <129573405+lorenzdiscutido@users.noreply.github.com>
2023-04-03 18:46:38 +0800

    Update activity problem 3.py

--- activity problem 3.py
+++ activity problem 3.py
@@ -1,3 +1,4 @@
+#This will generate a key and will return the keyword if it is the same length with the message
 def generateKey(message, key): 
   key = list(key) 
   if len(message) == len(key): 
@@ -7,6 +8,7 @@ def generateKey(message, key):
       key.append(key[i % len(key)]) 
   return("" . join(key)) 
 
+#This part of the code handles how to encrypt the message inputted
 def encryption(message, key):
     encrypted_text = []
     for i in range(len(message)):
@@ -15,9 +17,10 @@ def encryption(message, key):
         encrypted_text.append(chr(number_equivalent))
     return("" . join(encrypted_text))
 
+#This part will ask the user to input their message, keyword, and finally prints the encrypted message using vigenere code
 if __name__ == "__main__": 
-    message = input("Input a message you want to encrypt:")
-    keyword = input("Please input a keyword to encrypt the message:")
+    message = input("Input a message you want to encrypt(in all caps):")
+    keyword = input("Please input a keyword to encrypt the message(in all caps):")
     key = generateKey(message,keyword)
     encrypted_message = encryption(message, key)
     print("Message is", message)
