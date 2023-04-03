ac80456b0c2e9436313094919eaa68e7619b2f9d
lorenzdiscutido <129573405+lorenzdiscutido@users.noreply.github.com>
2023-04-03 18:45:35 +0800

    Update activity problem 2.py

--- activity problem 2.py
+++ activity problem 2.py
@@ -1,11 +1,11 @@
-#ask user to input a message to be decrypted
+#ask user to input a message
 encrypted_message = input(str("Please input an encrypted message"))
 message = ""
 
 #check each character
 for i in range(len(encrypted_message)):
     
-
+#This will change the symbol to their corresponding letter
     if encrypted_message[i] == "*":
         message += "a"
     elif encrypted_message[i] == "&":
@@ -18,5 +18,6 @@ for i in range(len(encrypted_message)):
         message += "u"
     else:
         message += encrypted_message[i]
-    
+
+#This will print the decrypted message   
 print(message) 
